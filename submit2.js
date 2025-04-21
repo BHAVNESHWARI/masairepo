7b6fd6d9cacf2fdc0bb50c3746232506f11ffbfd
bhavneshwari dadheech <bhavneshwaridadhich@gmail.com>
2025-04-21 21:35:20 +0530

    Add files via upload

---
+++ submit2.js
@@ -0,0 +1,12 @@
+
+
+  const employees = [
+    { name: "James", team: "A", performanceScores: [4, 5, 3] },
+    { name: "Sarah", team: "B", performanceScores: [2, 3, 4] },
+    { name: "Miguel", team: "A", performanceScores: [5, 4, 5] },
+    { name: "Priya", team: "B", performanceScores: [3, 2, 4] },
+    { name: "John", team: "C", performanceScores: [3, 3, 3] }
+  ]
+
+ console.log('employees.perforanceScores')
+
